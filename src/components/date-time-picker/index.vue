8d922cc10f079e31e21b64113e909348be78dda0
Rob Welan <robwelan@yahoo.com.au>
2017-09-05 09:03:56 +1000

    Create Build Assets
     - start of build

---
+++ src/components/date-time-picker/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ui input">
+        <input type="date" placeholder="Search...">
+    </div>
+</template>
+
+<script>
+//https://designmodo.com/calendar-jquery-css3/
+
+</script>
+
+<style>
+
+</style>
